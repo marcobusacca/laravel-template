5f41b805bfe8ccc5a369b7a5c01cfbeb18bd1b20
marcobusacca <marcobusacca01@gmail.com>
2023-09-01 18:04:40 +0200

    aggiornamento #2

--- resources/views/mails/new_email_contact.blade.php
+++ resources/views/mails/new_email_contact.blade.php
@@ -1,9 +1,9 @@
 <h1>Nuovo Contatto Ricevuto da Form</h1>
 <p>
-    Hai ricevuto un nuovo messaggio:
+    Hai ricevuto un nuovo messaggio:<br><br>
 
-    Utente: {{ $lead->name }}
-    Email: {{ $lead->email }}
+    Utente: {{ $lead->name }}<br><br>
+    Email: {{ $lead->email }}<br><br>
     Contenuto: <br>
     {{ $lead->content }}
 </p>
